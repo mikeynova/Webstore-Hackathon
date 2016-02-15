6916a1e8f437413701f97d119adbc8ea8f674a46
mikeynova <newfoundland7@gmail.com>
2016-02-15 00:51:45 -0800

    changes to hackathon html, along with new music html, new script.js, and new style.css pages

---
+++ script.js
@@ -0,0 +1,17 @@
+var dangBtn = function(){
+	alert("HEY THERE!");
+};
+
+$('button').click(dangBtn);
+
+var music = function(){
+	window.open(href="Music.html");
+};
+
+$('.active').click(music);
+
+var books = function(){
+	window.open(href="hackathon.html");
+};
+
+$('.navbar-brand').click(books);
