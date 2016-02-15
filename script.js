443b415ddeba3d47695d0b22bd80818f693ca99a
mikeynova <newfoundland7@gmail.com>
2016-02-15 10:12:17 -0800

    not using script.js, updated html docs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var music = function(){
-	window.open(href="Music.html");
-};
+// var music = function(){
+// 	window.open(href="Music.html");
+// };
 
-$('.active').click(music);
+// $('.active').click(music);
 
-var books = function(){
-	window.open(href="hackathon.html");
-};
+// var books = function(){
+// 	window.open(href="hackathon.html");
+// };
 
-$('.navbar-brand').click(books);
+// $('.navbar-brand').click(books);
